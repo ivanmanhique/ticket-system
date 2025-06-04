80ad4708606a76d10e80f4f54ec26c3f53dcb74d
Ivan Manhique <viriatomanhique@gmail.com>
2025-06-05 01:44:39 +0200

    added cors

--- src/createApp.ts
+++ src/createApp.ts
@@ -2,12 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import ticket_router from "./routes/ticket";
 import webhook_route from "./routes/webhook";
+import cors from 'cors';
 
 dotenv.config();
 
 
 const createApp = () => {
     const app = express();
+
+    app.use(cors({
+        origin: ['https://ticket-sys.youthmeraki.com'], // whitelist your domain
+        methods: ['GET', 'POST'],
+    }));
+
     app.use('/', webhook_route);
     app.use(express.json());
     app.use('/', ticket_router);
